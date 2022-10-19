8b00bdd870e0525c0c94ba548c290402cfd4b78d
Pablo5481 <pablo.samuel.coronel@gmail.com>
2022-10-18 22:28:33 -0400

    chapter update

--- scripts.js
+++ scripts.js
@@ -1,39 +1,83 @@
+const pokemonRepository = (function() {
+  const pokemonList = [];
+  const apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-function newItem(){
+function add(pokemon) {
+    pokemonList.push(pokemon);
+}
 
+function getAll() {
+    return pokemonList;
+}
 
+function addListItem(pokemon){
+    const pokemonList = document.querySelector(".pokemon-list");
+    const listPokemon = document.createElement("li");
+    const button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button");
+    button.setAttribute('data-toggle', 'modal');
+    button.setAttribute('data-target', '#pokemon-modal')
+    button.classList.add("btn")
+    listPokemon.appendChild(button);
+    pokemonList.appendChild(listPokemon);
+    button.addEventListener('click',function(event){
+      showDetails(pokemon);
+    });
+}
 
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
   
-    // jQuery Code
-    //1. Adding a new item to the list:
-    
-      let li = $('<li></li>');
-      let inputValue = $('#input').val();
-      li.append(inputValue);
-    
-      if (inputValue === '') {
-        alert("You must write something!");
-      } else {
-        $('#list').append(li);
-      }
-    //2. Crossing an item out:
-      function crossOut() {
-            li.toggleClass("strike");
-        }
-    
-        li.on("dblclick", function crossOut() {
-            li.toggleClass("strike");
-        });
-    //3. Adding a delete button
-      let crossOutButton = $('<crossOutButton></crossOutButton>');
-      crossOutButton.append(document.createTextNode('X'));
-      li.append(crossOutButton);
-    
-     crossOutButton.on("click", deleteListItem);
-       function deleteListItem(){
-             li.addClass("delete")
-         }
-       $('#list').sortable();
-    }
+function loadDetails(item) {
+    const url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.weight = details.weight;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+function showDetails(pokemon){
+    loadDetails(pokemon).then(function () {
+    showModal(pokemon);
+  });
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
     
     
+  };
+})();
+
+pokemonRepository.loadList().then(function() {
+  // Now the data is loaded!
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+  });
+});
